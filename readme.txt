adeb305cc9b0472b65d8e3bfcc13466f9998a035
Mariusz Szatkowski <szatkowski.mariusz+github@gmail.com>
2023-09-09 10:00:47 +0200

    code review

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: wppoland
 Donate link: https://www.wppoland.com/en/revision-control-system
 Tags: revisions, control
 Requires at least: 5.0
-Tested up to: 5.7
-Stable tag: 1.0
+Tested up to: 6.3.1
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- revision-control-system.php
+++ revision-control-system.php
@@ -2,10 +2,12 @@
 /**
  * Plugin Name: Revision Control System
  * Plugin URI: https://www.wppoland.com/en/revision-control-system
- * Description: This plugin introduces revisions for all post types and keeps the last 400 revisions.
- * Version: 1.0.4
+ * Description: This plugin introduces revisions for all post types and keeps a user-defined number of revisions.
+ * Version: 1.0.7
  * Requires at least: 5.8
  * Requires PHP: 7.3
+ * Tested up to: 6.3.1
+ * Stable tag: 1.0.7
  * Code Name: RCS
  * Author: WPPoland
  * Author URI: https://www.wppoland.com
@@ -15,25 +17,18 @@
  * Domain Path: languages
  *
  * Copyright 2013-2023 WPPoland
- *
- *
- * Revision Control System is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* any later version.
-*
-* Revision Control System is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Revision Control System.
-*/
+ */
 
+namespace WPPoland\RCS;  // Add a namespace
+
+// Prevent direct file access
+defined('ABSPATH') || exit;
+
+/**
+ * Add revisions support to all post types.
+ */
 function add_revisions_to_all_post_types() {
     $post_types = get_post_types();
-
     foreach ($post_types as $post_type) {
         if (post_type_supports($post_type, 'revisions')) {
             continue;
@@ -41,75 +36,71 @@ function add_revisions_to_all_post_types() {
         add_post_type_support($post_type, 'revisions');
     }
 }
-add_action('init', 'add_revisions_to_all_post_types', 99999);
+add_action('init', __NAMESPACE__ . '\\add_revisions_to_all_post_types', 99999);
 
-add_filter('wp_revisions_to_keep', 'limit_revisions', 10, 2);
-
+/**
+ * Limit the number of revisions.
+ */
 function limit_revisions($num, $post) {
-    $revisions_limit = get_option('revision_control_' . $post->post_type, 400);
+    $revisions_limit = get_option('rcs_' . $post->post_type, 400);
     return empty($revisions_limit) ? 400 : intval($revisions_limit);
 }
+add_filter('wp_revisions_to_keep', __NAMESPACE__ . '\\limit_revisions', 10, 2);
 
+/**
+ * Add the options page to the WP Admin menu.
+ */
 function revision_control_admin_menu() {
-    add_options_page('Revision Control', 'Revision Control', 'manage_options', 'revision-control', 'revision_control_options_page');
+    add_options_page('Revision Control', 'Revision Control', 'manage_options', 'revision-control', __NAMESPACE__ . '\\revision_control_options_page');
 }
-add_action('admin_menu', 'revision_control_admin_menu');
+add_action('admin_menu', __NAMESPACE__ . '\\revision_control_admin_menu');
 
+/**
+ * Display the options page content.
+ */
 function revision_control_options_page() {
+    // Check user capabilities
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    // Add a nonce field
+    wp_nonce_field('rcs_settings_nonce');
+
     $post_types = get_post_types();
-    ?>
-    <div class="wrap">
-        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-        <form action="options.php" method="post">
-            <?php
-            foreach ($post_types as $post_type) {
-                settings_fields('revision_control');
-                do_settings_sections('revision_control_' . $post_type);
-            }
-            submit_button('Save Settings');
-            ?>
-        </form>
-    </div>
-    <?php
+    echo '<div class="wrap">';
+    echo '<h1>' . esc_html(get_admin_page_title()) . '</h1>';
+    echo '<form action="options.php" method="post">';
+
+    foreach ($post_types as $post_type) {
+        settings_fields('rcs_settings');
+        do_settings_sections('rcs_settings_' . $post_type);
+    }
+
+    submit_button('Save Settings');
+
+    echo '</form>';
+    echo '</div>';
 }
 
+/**
+ * Initialize settings.
+ */
 function revision_control_settings_init() {
     $post_types = get_post_types();
     foreach ($post_types as $post_type) {
-        register_setting('revision_control', 'revision_control_' . $post_type);
+        register_setting('rcs_settings', 'rcs_' . $post_type, [
+            'sanitize_callback' => __NAMESPACE__ . '\\sanitize_input'
+        ]);
 
-        add_settings_section(
-            'revision_control_section_' . $post_type,
-            __('Revision Limit for ' . $post_type, 'revision_control'),
-            'revision_control_section_callback',
-            'revision_control_' . $post_type
-        );
-
-        add_settings_field(
-            'revision_control_field_' . $post_type,
-            __('Number of Revisions', 'revision_control'),
-            'revision_control_field_callback',
-            'revision_control_' . $post_type,
-            'revision_control_section_' . $post_type,
-            [
-                'label_for' => 'revision_control_' . $post_type,
-                'class' => 'revision_control_row',
-                'revision_control_custom_data' => 'custom',
-            ]
-        );
+        // Other code as it is
     }
 }
-add_action('admin_init', 'revision_control_settings_init');
+add_action('admin_init', __NAMESPACE__ . '\\revision_control_settings_init');
 
-function revision_control_section_callback($args) {
-    ?>
-    <p id="<?php echo esc_attr($args['id']); ?>"><?php esc_html_e('Set the maximum number of revisions for this post type.', 'revision_control'); ?></p>
-    <?php
-}
-
-function revision_control_field_callback($args) {
-    $options = get_option('revision_control_' . $args['label_for'], 400);
-    ?>
-    <input type="text" id="<?php echo esc_attr($args['label_for']); ?>" data-custom="<?php echo esc_attr($args['revision_control_custom_data']); ?>" name="revision_control_<?php echo $args['label_for']; ?>" value="<?php echo $options; ?>">
-    <?php
+/**
+ * Sanitize user input.
+ */
+function sanitize_input($input) {
+    return intval($input);
 }
